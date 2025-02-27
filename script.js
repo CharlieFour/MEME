e9be0c2996af61cf177600089d3393957fc59fa1
Abdul Rafay <rafay0820188@gmail.com>
2025-02-27 12:11:52 +0500

    adding details and logo of coin, template

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-document.addEventListener("DOMContentLoaded", function ()
-{
+document.addEventListener("DOMContentLoaded", function () {
     const socials = document.querySelector(".socials");
     
-    socials.addEventListener("animationiteration", () =>
-        {
+    socials.addEventListener("animationiteration", () => {
         socials.style.animation = "none";
-        setTimeout(() =>
-        {
+        setTimeout(() => {
             socials.style.animation = "roll 10s linear infinite";
         }, 10);
     });
